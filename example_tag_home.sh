99056c412bb65769554404b0e23f466eea62898f
Joerg Rossdeutscher <j.rossdeutscher@superreal.de>
2014-11-05 14:46:59 +0100

    Cleanup
    Push latest changes

--- example_tag_home.sh
+++ example_tag_home.sh
@@ -1,4 +1,5 @@
 
 
+#exiftool -coordFormat '%.6f' -exif:gpslatitude -exif:gpslongitude  /Users/jrossdeutscher/Desktop/Neuer\ Ordner\ 4/IMG_20141015_160229.jpg
 
 # exiftool -overwrite_original -preserve -exif:gpslatitude=53.567903 -exif:gpslongitude=9.896196  /Volumes/Fettplatte1000/Fotos/2014_08_09_Canon/IMG_5893.JPG
